7f1c4d0835202abcb6f7bf42a3a8813dc48a8e0c
Dale Stimson <dale.b.stimson@intel.com>
2014-09-09 08:23:45 +0800

    ION support in psb_video
    when ion is enabled as backend of gralloc.
    video driver needs to pass down fd of graphics buffer
    when creat surface from it.
    
    Orig-Author:     Randy Xu <randy.xu@intel.com>
    
    BZ: 224134
    Bug: 17316531
    Depends-on: Ife9513fe58e06ea2829987f328668f975a44b4e0
    Depends-on: I4b8422d48b06419af6e48b631bacbe6c7169574e
    Depends-on: Ia4c5bc80df952024a0973f2bacc42992301dfbf9
    Depends-on: I172cdcb8cc86fd6fa3d3adadbab19b6fd3e6a18e
    Depends-on: I2a2e721719349f755033c15bdeb732e7fb5c89ac
    Depends-on: I48deabb107cc3abfa59eff5e012b51293fe6d74f
    Depends-on: If2be266fc9063d7a2b701f85e8ace26aaf484ff5
    Change-Id: I0ca0f06491e3fb498b3b41b70cbe1b6cf39e1918
    Signed-off-by: mahongpe <hongpeng.ma@intel.com>

--- src/android/psb_surface_gralloc.c
+++ src/android/psb_surface_gralloc.c
@@ -199,9 +199,10 @@ VAStatus psb_CreateSurfacesFromGralloc(
             vaStatus = VA_STATUS_ERROR_UNKNOWN;
         } else {
             int cache_flag = PSB_USER_BUFFER_UNCACHED;
+            int buf_fd = gralloc_getbuffd(handle);
 
             vaStatus = psb_surface_create_from_ub(driver_data, width, height, fourcc,
-                    external_buffers, psb_surface, vaddr,
+                    external_buffers, psb_surface, vaddr, buf_fd,
                     cache_flag);
 
             psb_surface->buf.handle = handle;
@@ -367,12 +368,13 @@ VAStatus psb_CreateSurfacesFromGralloc(
             vaStatus = VA_STATUS_ERROR_UNKNOWN;
         } else {
             int cache_flag = PSB_USER_BUFFER_UNCACHED;
+            int buf_fd = gralloc_getbuffd((buffer_handle_t)handle);
 #ifdef PSBVIDEO_MRFL
             cache_flag = 0;
 #endif
             vaStatus = psb_surface_create_from_ub(driver_data, width, height, fourcc,
                     (VASurfaceAttributeTPI *)external_buffers, psb_surface,
-                    vaddr[GRALLOC_SUB_BUFFER0], cache_flag);
+                    vaddr[GRALLOC_SUB_BUFFER0], buf_fd, cache_flag);
             psb_surface->buf.handle = (void *)handle;
             obj_surface->share_info = NULL;
 
--- src/psb_surface_attrib.c
+++ src/psb_surface_attrib.c
@@ -55,6 +55,7 @@ VAStatus psb_surface_create_from_ub(
     VASurfaceAttributeTPI *graphic_buffers,
     psb_surface_p psb_surface, /* out */
     void *vaddr,
+    int fd,
     unsigned flags
 )
 {
@@ -118,12 +119,12 @@ VAStatus psb_surface_create_from_ub(
     if (graphic_buffers->tiling)
         ret = psb_buffer_create_from_ub(driver_data, psb_surface->size,
                 psb_bt_mmu_tiling, &psb_surface->buf,
-                vaddr, 0);
+                vaddr, fd, 0);
     else
 #endif
         ret = psb_buffer_create_from_ub(driver_data, psb_surface->size,
                 psb_bt_surface, &psb_surface->buf,
-                vaddr, flags);
+                vaddr, fd, flags);
 
     return ret ? VA_STATUS_ERROR_ALLOCATION_FAILED : VA_STATUS_SUCCESS;
 }
@@ -601,10 +602,10 @@ VAStatus  psb_CreateSurfaceFromUserspace(
 
         if (attribute_tpi->type == VAExternalMemoryNoneCacheUserPointer)
             vaStatus = psb_surface_create_from_ub(driver_data, width, height, fourcc,
-                    attribute_tpi, psb_surface, vaddr, PSB_USER_BUFFER_UNCACHED);
+                    attribute_tpi, psb_surface, vaddr, -1, PSB_USER_BUFFER_UNCACHED);
         else
             vaStatus = psb_surface_create_from_ub(driver_data, width, height, fourcc,
-                    attribute_tpi, psb_surface, vaddr, 0);
+                    attribute_tpi, psb_surface, vaddr, -1,  0);
         obj_surface->psb_surface = psb_surface;
 
         if (VA_STATUS_SUCCESS != vaStatus) {
@@ -721,7 +722,7 @@ VAStatus  psb_CreateSurfaceFromION(
         }
 
         vaStatus = psb_surface_create_from_ub(driver_data, width, height, fourcc,
-                attribute_tpi, psb_surface, vaddr, 0);
+                attribute_tpi, psb_surface, vaddr, ion_source_share.fd, 0);
         obj_surface->psb_surface = psb_surface;
 
         if (VA_STATUS_SUCCESS != vaStatus) {
